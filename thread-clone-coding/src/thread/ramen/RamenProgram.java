05bf48b3351bc03436d3c6b0f44580ab804fc79a
KimJangHyeon <kimjangh94@gmail.com>
2021-07-21 00:05:35 +0900

    .

---
+++ thread-clone-coding/src/thread/ramen/RamenProgram.java
@@ -0,0 +1,32 @@
+package thread.ramen;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+
+public class RamenProgram {
+
+	public static void main(String[] args) throws Exception {
+		int odrRamenCnt;
+		
+		System.out.println("라면 몇 개 끓일까요?");
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		odrRamenCnt = Integer.parseInt(br.readLine());
+		
+		System.out.println(odrRamenCnt + "개 주문 완료! 조리시작!");
+		
+		
+		try{
+			RamenCook ramenCook = new RamenCook(odrRamenCnt);
+			new Thread(ramenCook,"A").start();
+			new Thread(ramenCook,"B").start();
+			new Thread(ramenCook,"C").start();
+			new Thread(ramenCook,"D").start();
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		
+	}
+
+}
